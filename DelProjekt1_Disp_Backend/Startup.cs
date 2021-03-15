019c80fc8874cf95c9df8c0e5f563e454e3c7262
Michael Janum Davidsen <au600586@uni.au.dk>
2021-03-15 14:44:07 +0100

    Migration and DB connection

--- DelProjekt1_Disp_Backend/Startup.cs
+++ DelProjekt1_Disp_Backend/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using DelProjekt1_Disp_Backend.Data;
 
 namespace DelProjekt1_Disp_Backend
 {
@@ -26,12 +28,16 @@ namespace DelProjekt1_Disp_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+         
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DelProjekt1_Disp_Backend", Version = "v1" });
             });
+
+            services.AddDbContext<DelProjekt1_Disp_BackendContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DelProjekt1_Disp_BackendContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
